28a13c8c8503d10f96d21056e53ee140205f1238
Daniel Peter <curveballdaniel@gmail.com>
2024-05-30 13:32:19 -0400

    Begin toying with env variables and labels on volume

--- pkg/blockchain/bitcoin/bitcoin.go
+++ pkg/blockchain/bitcoin/bitcoin.go
@@ -15,6 +15,14 @@ type Bitcoin struct{}
 func (b Bitcoin) StartNode(config config.Config) error {
 	fmt.Println("Starting Bitcoin node with config:", config)
 
+	// Set environment variables for Docker Compose
+	//os.Setenv("BITCOIN_PORT", fmt.Sprintf("%d", config.Port))
+	//os.Setenv("BITCOIN_STORAGE_PATH", config.StoragePath)
+	//os.Setenv("BITCOIN_EXTRA_ARGS", fmt.Sprintf("%s", extraArgs))
+
+	//     command: bitcoind ${BITCOIN_EXTRA_ARGS}
+	//  - "${FIFTYSIX_BITCOIN_RPC_PORT:-8332}:${FIFTYSIX_BITCOIN_RPC_PORT:-8332}"
+
 	// Define the path to the Docker Compose file
 	composeFilePath := "docker/bitcoin/docker-compose_bitcoin-core.yml"
 

601ba82bba01c950346032ec4b47c87a4cb18e85
Daniel Peter <curveballdaniel@gmail.com>
2024-06-20 10:39:51 -0400

    Remove ethereum config settings

--- pkg/docker/compose/network_compose_mappings.go
+++ pkg/docker/compose/network_compose_mappings.go
@@ -26,8 +26,8 @@ func GetNetworkComposeConfig(network string) (NetworkConfig, error) {
 	switch {
 	case network == "bitcoin" || network == "bitcoin-testnet":
 		return GetBitcoinNetworkComposeConfig(network)
-	case network == "ethereum":
-		return GetEthereumNetworkComposeConfig(network)
+	//case network == "ethereum":
+	//return GetEthereumNetworkComposeConfig(network)
 	default:
 		return NetworkConfig{}, fmt.Errorf("unknown network: %s", network)
 	}

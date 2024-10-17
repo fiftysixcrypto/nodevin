f491f6c604b56315bf5149e6caac9baf2c56ad92
Daniel Peter <curveballdaniel@gmail.com>
2024-10-16 20:55:54 -0400

    Refactor cleanup to work on Windows

--- pkg/blockchain/cleanup.go
+++ pkg/blockchain/cleanup.go
@@ -1,8 +1,10 @@
 package blockchain
 
 import (
+	"bytes"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/fiftysixcrypto/nodevin/internal/logger"
 	"github.com/spf13/cobra"
@@ -19,13 +21,38 @@ var cleanupCmd = &cobra.Command{
 func cleanupAllImages() {
 	logger.LogInfo("Removing all Docker images starting with 'fiftysix/'...")
 
-	removeCmd := exec.Command("sh", "-c", "docker images --format '{{.Repository}}:{{.Tag}}' | grep '^fiftysix/' | xargs -r docker rmi")
-	removeCmd.Stdout = os.Stdout
-	removeCmd.Stderr = os.Stderr
+	// Get Docker images
+	listCmd := exec.Command("docker", "images", "--format", "{{.Repository}}:{{.Tag}}")
+	var out bytes.Buffer
+	listCmd.Stdout = &out
+	listCmd.Stderr = os.Stderr
 
-	if err := removeCmd.Run(); err != nil {
-		logger.LogError("Failed to remove Docker images: " + err.Error())
+	if err := listCmd.Run(); err != nil {
+		logger.LogError("Failed to list Docker images: " + err.Error())
 		return
+	}
+
+	// Filter images that start with 'fiftysix/'
+	images := strings.Split(out.String(), "\n")
+	var filteredImages []string
+	for _, image := range images {
+		if strings.HasPrefix(image, "fiftysix/") {
+			filteredImages = append(filteredImages, image)
+		}
+	}
+
+	// Remove filtered images
+	if len(filteredImages) > 0 {
+		logger.LogInfo("Removing images: " + strings.Join(filteredImages, ", "))
+		removeCmd := exec.Command("docker", append([]string{"rmi"}, filteredImages...)...)
+		removeCmd.Stdout = os.Stdout
+		removeCmd.Stderr = os.Stderr
+		if err := removeCmd.Run(); err != nil {
+			logger.LogError("Failed to remove Docker images: " + err.Error())
+			return
+		}
+	} else {
+		logger.LogInfo("No Docker images starting with 'fiftysix/' found.")
 	}
 
 	logger.LogInfo("Successfully removed all Docker images starting with 'fiftysix/'.")
